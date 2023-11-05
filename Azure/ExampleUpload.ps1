ed8592dd9633ee72af49c1370dd47bac9baaa502
rgoldstein1989 <rgoldstein@itsavvy.com>
2023-11-05 01:17:30 -0400

    added_example
    can use example data now.

---
+++ Azure/ExampleUpload.ps1
@@ -0,0 +1,5 @@
+﻿import-module C:\GitHub\Microsoft365\Azure\upload-table.ps1 -Force
+$mxcheck = Import-Csv C:\GitHub\Microsoft365\ExampleData\MXCheck.csv
+
+upload-table "testTable01" "MXData" $mxcheck
+
